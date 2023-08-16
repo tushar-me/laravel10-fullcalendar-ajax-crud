5a85e55965b3e1aa820a5e5db892d1759ad2a8b8
TusharImran-Dev <tusharimran.dev@gmail.com>
2023-08-16 06:18:34 +0600

    Laravel 10 FullCalendar Ajax CRUD

---
+++ app/Http/Controllers/FullCalendarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class FullCalendarController extends Controller
+{
+    
+    public function index(Request $request)
+    {
+
+        if ($request->ajax()) {
+
+            $data = Event::whereDate('start', '>=', $request->start)
+                ->whereDate('end',   '<=', $request->end)
+                ->get(['id', 'title', 'start', 'end']);
+
+            return response()->json($data);
+        }
+
+        return view('fullcalendar');
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function ajax(Request $request)
+    {
+
+        switch ($request->type) {
+            case 'add':
+                $event = Event::create([
+                    'title' => $request->title,
+                    'start' => $request->start,
+                    'end' => $request->end,
+                ]);
+
+                return response()->json($event);
+                break;
+
+            case 'update':
+                $event = Event::find($request->id)->update([
+                    'title' => $request->title,
+                    'start' => $request->start,
+                    'end' => $request->end,
+                ]);
+
+                return response()->json($event);
+                break;
+
+            case 'delete':
+                $event = Event::find($request->id)->delete();
+
+                return response()->json($event);
+                break;
+
+            default:
+                # code...
+                break;
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FullCalendarController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', [FullCalendarController::class, 'index']);
+Route::post('/fullcalendar-ajax', [FullCalendarController::class, 'ajax']);
